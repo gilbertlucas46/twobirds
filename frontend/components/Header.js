ece275d9d6b9cc13ee6655208870a5f5f5a5911c
Gilbert <gilbertlucas46@gmail.com>
2019-04-09 15:53:28 +0800

    fixed style flicker on server render

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,15 +1,64 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+import NProgress from 'nprogress';
+import Router from 'next/router';
 import Nav from './Nav';
 
 
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+    NProgress.done();
+};
+
+
+const Logo = styled.h1`
+    font-size:4rem;
+    margin-left:2rem;
+    position: relative;
+    z-index:2;
+    transform: skew(-7deg);
+    a {
+        text-decoration:none;
+    }
+`;
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${props => props.theme.black};
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: stretch;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${props => props.theme.lightgrey};
+  }
+`;
+
 const Header = () => (
-    <div>
+    <StyledHeader>
         <div className="bar">
-            <a href="">Wedding Planner</a>
+            <Logo>
+                <Link href="/">
+                    <a>Two Birds</a>
+                </Link>
+            </Logo>
             <Nav/>
         </div>
         <div className="sub-bar">
             <p>Search</p>
         </div>
-    </div>
+    </StyledHeader>
 )
 export default Header;
---
+++ frontend/pages/about.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+      return (
+        <div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae pariatur quos ab maxime explicabo facilis sint veniam atque nisi et porro earum, doloremque, inventore ratione at mollitia aut ipsum quas.</p>
+        </div>
+      )
+    }
+  }
+  export default About;
